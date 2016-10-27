e55e8f55429248e65fae58cd6b7b0ff3766b5737
DIYman <DIYman@users.noreply.github.com>
2016-10-27 12:29:42 +0300

    Update AirlineRes.java

--- 7.19/AirlineRes.java
+++ 7.19/AirlineRes.java
@@ -4,8 +4,6 @@ public class AirlineReservationSystem{
     boolean[] arrSeats = new boolean[10];
     Scanner sc = new Scanner(System.in);
 
-    // SETTERS
-    // assigns first empty seat in relevant section
     public boolean assignSeat(String section){
         if(section == "first"){
             if(getFreeSeats(section) > 0){
@@ -29,8 +27,7 @@ public class AirlineReservationSystem{
 
             }
         }
-        // seats in chosen section full
-        // check if ok to assign to other section
+
         System.out.printf("All seats in section \"%s\" are booked.\n", section);
         System.out.printf("Would you like to be moved to section \"%s\" (y/n)? ",
                 (section == "first") ? "economy" : "first");
@@ -42,8 +39,7 @@ public class AirlineReservationSystem{
 
         return false;
     }
-    // GETTERS
-    // returns number of free seats in each section
+
     private int getFreeSeats(String section){
         int total = 0;
         if(section == "first"){
@@ -53,7 +49,6 @@ public class AirlineReservationSystem{
                     total += 1;
             }
         }else if(section == "economy"){
-            // economy 6-10 (array 5-9)
             for(int i=5; i<arrSeats.length; i++){
                 if(arrSeats[i] == false)
                     total += 1;
@@ -61,21 +56,18 @@ public class AirlineReservationSystem{
         }
         return total;
     }
-    // see whether or not all seats are booked
+
     public boolean seatsAvailable(){
-        // if empty seat found return true
         for(boolean seat : arrSeats)
             if(seat == false)
                 return true;
 
-        // if none found plane is full
         return false;
     }
 
     public void printGreeting(){
         System.out.println("\nWelcome to Crap Airlines booking system.\n");
     }
-    // print the menu with remaining number of seats for each section
     public void printMenu(){
         System.out.printf("1. Economy class %s\n",
                 (getFreeSeats("economy") > 0) ?
@@ -85,7 +77,7 @@ public class AirlineReservationSystem{
                  "(" + Integer.toString(getFreeSeats("first")) + ")" : "(full)"));
         System.out.print("> ");
     }
-    // prints the boarding pass
+
     private void printBoardingPass(int seat){
         System.out.println("\nBoarding pass for Crap Airlines.");
         System.out.printf("\nSECTION: %s\nSEAT NUMBER: %d\n\n\n",
